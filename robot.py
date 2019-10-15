5091be5be7e45db55c114bd4866d6db12021648e
BlockBeasts <kjosien1@gmail.com>
2019-10-14 18:34:08 -0600

    add run until stucked & edited crane for reliability

--- robot.py
+++ robot.py
@@ -129,3 +129,9 @@ class Robot:
             self.motorC.run(0)
         self.motorB.run(0)
         self.motorC.run(0)
+
+    def runUntilStucked(self, motor, speed, direction):
+        if direction == 1:
+            motor.run_until_stalled(speed, Stop.COAST, 100)
+        elif direction == -1:
+            motor.run_until_stalled(-speed, Stop.COAST, 100)
